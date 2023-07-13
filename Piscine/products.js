d4736275997c36002227ad87ddef16892fd8be1a
abdallah44788 <105983491+abdallah44788@users.noreply.github.com>
2023-07-13 19:27:09 +0100

    Add files via upload

---
+++ Piscine/products.js
@@ -0,0 +1,17 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.jpg',
+		image3: 'img/products/3.jpg',
+		title: 'حوض السباحة للأطفال قابل للنفخ للانتعاش والمرح في أجواء الصيف الحارة',
+		price1: 6000,
+		price: 4450,
+		description: '',
+		
+		phone: 0562057742,
+	},
+
+	
+];
