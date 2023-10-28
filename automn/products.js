840f72bf28d554843ae80fad48d38bed0bfdc9b4
abdallah44788 <105983491+abdallah44788@users.noreply.github.com>
2023-10-29 00:37:11 +0100

    Add files via upload

---
+++ automn/products.js
@@ -0,0 +1,19 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.jpg',
+		image2: 'img/products/2.jpg',
+		image3: 'img/products/3.jpg',
+		image4: 'img/products/4.jpg',
+		image5: 'img/products/5.jpg',
+
+		title: 'أطلبيها الان واستمتعي بأجمل اطلالة خريفية (الكمية جد محدودة)  ',
+		price1: 4850,
+		price: 3600,
+		description: 'احصلي الان على أجمل اطلالة خريفية و المصنوعة بجودة عالية وبقماش ممتاز .متوفرة بعدة ألوان واقياس .بالاضافة لهذا فانها مصنوعة بجودة عالية ومناسبة لكل خرجاتك . لاتضيعي الفرصة وأطلبيها الان فالكمية جد محدودة 		'   ,
+		phone: 0562057742,
+	},
+
+	
+];
